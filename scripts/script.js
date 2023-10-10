aa85f7c5d62e3a5bdea574b315ee8923fe8eaa63
Ukeje Cole <tatianawoodruff00@gmail.com>
2023-10-10 00:57:11 -0400

    index.html update?

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// Fill up Footer
+const curDate = new Date();
+const year = curDate.getFullYear();
+let lastModifiedDate = new Date(document.lastModified);
+
+let modYear = lastModifiedDate.getFullYear();
+let modDay = lastModifiedDate.getDay();
+let modMonth = lastModifiedDate.getMonth();
+let modminute = lastModifiedDate.getMinutes();
+let modsecond = lastModifiedDate.getSeconds()
+let modhour = lastModifiedDate.getHours(); 
+
+footerHead.insertAdjacentHTML("afterbegin", `&#169 ${year} | Ukeje Cole | Nigeria`);
+foooterP.insertAdjacentHTML("afterbegin", `Last Updated: ${modMonth}/${modDay}/${modYear} ${modhour}:${modminute}:${modsecond} `)
